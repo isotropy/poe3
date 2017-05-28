579244e21846d251f9cf99c4ce79e9f24ed642d3
arivanandan <uarivanandan@gmail.com>
2017-05-28 18:14:04 +0530

    Updates home

--- src/ui/actions/home.jsx
+++ src/ui/actions/home.jsx
@@ -2,7 +2,7 @@ import * as homeAPI from "../../server/home";
 import { updateState } from "redux-jetpack";
 
 export async function getLatest(user) {
-  const results = await homeAPI.getLatest({ id: 2 });
+  const results = await homeAPI.getLatest({ id: 1 });
 
   updateState("home", state => ({
     ...state,
--- src/ui/components/home/index.jsx
+++ src/ui/components/home/index.jsx
@@ -9,8 +9,9 @@ class Home extends Component {
   }
 
   render() {
-    return <Posts posts={this.props.posts} />;
+    if (this.props.posts.length > 0) return <Posts posts={this.props.posts} />;
+    return <div>Head over to Explore and follow someone. We will fill up this page with haikus you'd love.</div>
   }
 };
 
-export default connect(Home, state => state.home);
+export default connect(Home, state => state.home);</div>
