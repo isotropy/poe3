01c468a190832052bc3f450c76b90f8b1a03d18a
Jeswin Kumar <jeswinpk@agilehead.com>
2017-06-11 09:27:18 +0530

    Check for null in comments before reducing

--- src/ui/actions/comments.js
+++ src/ui/actions/comments.js
@@ -1,24 +1,23 @@
-import * as commentsAPI from '../../server/comments';
-import * as profileActions from './profile';
-import { getState, updateState } from 'redux-jetpack';
-import ramda from 'ramda';
+import * as commentsAPI from "../../server/comments";
+import * as profileActions from "./profile";
+import { getState, updateState } from "redux-jetpack";
+import ramda from "ramda";
 
 export async function getLatest(postId) {
   let ungroupedComments = await commentsAPI.getLatest(postId);
 
-  const comments = ramda.groupBy(c => c.parentCommentId || 'root')(
-    ungroupedComments
-  );
+  const comments = ramda.groupBy(c => c.parentCommentId || "root")(ungroupedComments);
 
-  const groupedComments = comments.root.reduce(
-    (acc, comment) =>
-      acc.concat({ ...comment, children: comments[comment.id] }),
-    []
-  );
+  const groupedComments = comments.root
+    ? comments.root.reduce(
+        (acc, comment) => acc.concat({ ...comment, children: comments[comment.id] }),
+        []
+      )
+    : [];
 
   if (getState().comments.commentsIsOpen === postId) postId = null;
 
-  updateState('comments', state => ({
+  updateState("comments", state => ({
     ...state,
     comments: groupedComments,
     commentsIsOpen: postId
